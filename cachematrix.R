bcd8d31e4fc91c690bd2280e96f5a1239d52aba3
Pamela Milani <pame.milani@gmail.com>
2016-05-21 17:12:50 -0700

    Implemented matrix with cached invere

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(M = matrix(1)) {
+        # Initialize the inverse to null
+        inverse <- NULL
+        set <- function(M1) {
+                M <<- M1
+                # The matrix is changed so any previously computed
+                # inverse is invalid, reset to null
+                inverse <<- NULL
+        }
+        get <- function() M
+        setInverse <- function(inverse_1) inverse <<- inverse_1
+        getInverse <- function() inverse
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
+cacheSolve <- function(M) {
+        inverse <- M$getInverse()
+        if(!is.null(inverse)) {
+                # We already computed this invers, return the cached
+                # value
+                message("getting cached data")
+                return(inverse)
+        }
+        data <- M$get()
+        inverse <- solve(data)
+        M$setInverse(inverse)
+        inverse
+}
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# The same cache logic can also be implemented as a single object:
+matrixWithInverse <- function(M = matrix()) 
+{
+        inverse <- NULL
+        set <- function(M1) {
+                M <<- M1
+                inverse <<- NULL
+        }
+        get <- function() M
+        getInverse <- function(){
+            if(is.null(inverse)){
+                inverse <<- solve(M)
+            }
+            else{
+                message("using cached data")
+            }
+            inverse
+        }
+        list(set = set, get = get, getInverse = getInverse)
 }
